0da8b9f59ec7ca36cfc71b7a3d83cd241a3546e4
Leoar P. Cajes Jr. <xyrelkyut0510@gmail.com>
2025-02-17 12:36:01 +0800

    Create Part 1: PersonalDetails.java

---
+++ Part 1: PersonalDetails.java
@@ -0,0 +1,15 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        String name = "Leoar P. Cajes Jr.";
+        int age = 19;
+        double height = 175;
+        boolean isStudent = true;
+
+        // Displaying the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " centimeters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
